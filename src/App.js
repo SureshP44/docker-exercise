e7276b2fcc197765abe4f48b04a45df2a6815701
Suresh Perumal <sureshperumal@Sureshs-MacBook-Pro.local>
2021-04-29 21:58:10 +0530

    added travis cli

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          bye da <code>src/App.js</code> and save to reload.
+          hii da <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
